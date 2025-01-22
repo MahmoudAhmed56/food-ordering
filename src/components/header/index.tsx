f220b65ea476b7abab35388c3e19f95f5d122924
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-01-22 14:53:01 +0200

    add navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,21 @@
+import { Routes } from "@/constants/enums";
+import Link from "../link";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header>Header</header>
-  )
-}
+    <header className="py-4 md:py-6">
+      <div className="container flex items-center justify-between">
+        <Link
+          className="text-primary font-semibold text-2xl"
+          href={Routes.ROOT}
+        >
+          Logo
+        </Link>
+        <Navbar />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
