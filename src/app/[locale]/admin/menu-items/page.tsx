928c027c821eca6c48adbd63ae1226264dda9eaf
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-12 21:05:34 +0200

    fix some errors

--- src/app/[locale]/admin/menu-items/page.tsx
+++ src/app/[locale]/admin/menu-items/page.tsx
@@ -4,21 +4,23 @@ import { Languages, Pages, Routes } from "@/constants/enums";
 import { Locale } from "@/i18n.config";
 import getTrans from "@/lib/translation";
 import { authOptions } from "@/server/auth";
+import { getProducts } from "@/server/db/products";
 import { UserRole } from "@prisma/client";
 import { ArrowRightCircle } from "lucide-react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import React from "react";
+import MenuItems from "./_components/MenuItems";
 
-const MenuItemsPage = async ({
+async function MenuItemsPage({
   params,
 }: {
   params: Promise<{ locale: Locale }>;
-}) => {
+}) {
   const locale = (await params).locale;
   const translations = await getTrans(locale);
   const session = await getServerSession(authOptions);
   const products = await getProducts();
+
   if (!session) {
     redirect(`/${locale}/${Routes.AUTH}/${Pages.LOGIN}`);
   }
@@ -29,26 +31,26 @@ const MenuItemsPage = async ({
 
   return (
     <main>
-    <section className="section-gap">
-      <div className="container">
-        <Link
-          href={`/${locale}/${Routes.ADMIN}/${Pages.MENU_ITEMS}/${Pages.NEW}`}
-          className={`${buttonVariants({
-            variant: "outline",
-          })} !mx-auto !flex !w-80 !h-10 mb-8`}
-        >
-          {translations.admin["menu-items"].createNewMenuItem}
-          <ArrowRightCircle
-            className={`!w-5 !h-5 ${
-              locale === Languages.ARABIC ? "rotate-180 " : ""
-            }`}
-          />
-        </Link>
-        <MenuItems products={products} />
-      </div>
-    </section>
-  </main>
+      <section className="section-gap">
+        <div className="container">
+          <Link
+            href={`/${locale}/${Routes.ADMIN}/${Pages.MENU_ITEMS}/${Pages.NEW}`}
+            className={`${buttonVariants({
+              variant: "outline",
+            })} !mx-auto !flex !w-80 !h-10 mb-8`}
+          >
+            {translations.admin["menu-items"].createNewMenuItem}
+            <ArrowRightCircle
+              className={`!w-5 !h-5 ${
+                locale === Languages.ARABIC ? "rotate-180 " : ""
+              }`}
+            />
+          </Link>
+          <MenuItems products={products} />
+        </div>
+      </section>
+    </main>
   );
-};
+}
 
 export default MenuItemsPage;
